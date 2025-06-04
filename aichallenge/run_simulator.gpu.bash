2c2d55916ce01846341a0083a3ad2b35321ab9ca
banban1029 <banshiro1029@gmail.com>
2025-06-04 12:57:59 +0900

    adapt gpu

---
+++ aichallenge/run_simulator.gpu.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+AWSIM_DIRECTORY=/aichallenge/simulator/AWSIM_GPU
+
+mode="${1}"
+
+case "${mode}" in
+"endless")
+    opts=("--endless")
+    ;;
+*)
+    opts=("")
+    ;;
+esac
+
+# shellcheck disable=SC1091
+source /aichallenge/workspace/install/setup.bash
+sudo ip link set multicast on lo
+sudo sysctl -w net.core.rmem_max=2147483647 >/dev/null
+$AWSIM_DIRECTORY/AWSIM.x86_64 "${opts[@]}"
